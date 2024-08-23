a2bec67043c4fb3f85a9f613b433bf205c33b14f
jmlee2147 <jm2147@khu.ac.kr>
2024-08-23 23:35:24 +0900

    웹소켓 -> 롱 폴링 방식으로 변경

--- app.py
+++ app.py
@@ -7,33 +7,35 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import timedelta
 from flask_mail import Mail, Message
 from flask_socketio import SocketIO
+from collections import defaultdict
 
 import re
 import os
 import random
-import string
+import time
+import threading
 
-app = Flask(__name__)
-socketio = SocketIO(app)
+application = Flask(__name__)
+socketio = SocketIO(application)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:2147@localhost/userinfo'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY', 'default_secret_key')
-app.config['REMEMBER_COOKIE_DURATION'] = timedelta(days=1)
+application.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:2147@localhost/userinfo'
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+application.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY', 'default_secret_key')
+application.config['REMEMBER_COOKIE_DURATION'] = timedelta(days=1)
 
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'jmlee2147@gmail.com'
-app.config['MAIL_PASSWORD'] = 'irwjgpldnvmlzkxd'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
-mail = Mail(app)
+application.config['MAIL_SERVER'] = 'smtp.gmail.com'
+application.config['MAIL_PORT'] = 465
+application.config['MAIL_USERNAME'] = 'jmlee2147@gmail.com'
+application.config['MAIL_PASSWORD'] = 'irwjgpldnvmlzkxd'
+application.config['MAIL_USE_TLS'] = False
+application.config['MAIL_USE_SSL'] = True
+mail = Mail(application)
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+db = SQLAlchemy(application)
+migrate = Migrate(application, db)
 
 login_manager = LoginManager()
-login_manager.init_app(app)
+login_manager.init_app(application)
 login_manager.login_view = 'login'
 
 class Team(db.Model):
@@ -79,7 +81,7 @@ def is_valid_password(password):
         return False
     return True
 
-@app.route('/signup_data', methods=['GET', 'POST'])
+@application.route('/signup_data', methods=['GET', 'POST'])
 def signup_data():
     if request.method == 'POST':
         username = request.form.get('username')
@@ -134,7 +136,7 @@ def signup_data():
 
     return render_template("signup_data.html")
 
-@app.route('/login', methods=['GET', 'POST'])
+@application.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         userid = request.form.get('userid')
@@ -151,7 +153,7 @@ def login():
 
     return render_template('login.html')
 
-@app.route('/profile', methods=['GET', 'POST'])
+@application.route('/profile', methods=['GET', 'POST'])
 @login_required
 def profile():
     if request.method == 'POST':
@@ -166,38 +168,38 @@ def profile():
         return redirect(url_for('profile'))
     return render_template('profile.html', gender=current_user.gender, profile_picture=current_user.profile_picture)
 
-@app.route('/logout')
+@application.route('/logout')
 @login_required
 def logout():
     logout_user()
     session.pop('_flashes', None)
     return redirect(url_for('start'))
 
-@app.route('/')
+@application.route('/')
 def start():
     return render_template('start.html')
 
-@app.route('/signup_1')
+@application.route('/signup_1')
 def signup_1():
     return render_template('signup_1.html')
 
-@app.route('/signup_2')
+@application.route('/signup_2')
 def signup_2():
     return render_template('signup_2.html')
 
-@app.route('/signup_3')
+@application.route('/signup_3')
 def signup_3():
     return render_template('signup_3.html')
 
-@app.route('/signup_4')
+@application.route('/signup_4')
 def signup_4():
     return render_template('signup_4.html')
 
-@app.route('/signup_5')
+@application.route('/signup_5')
 def signup_5():
     return render_template('signup_5.html')
 
-@app.route('/index')
+@application.route('/index')
 @login_required
 def index():
     user = current_user
@@ -205,12 +207,12 @@ def index():
     team_members = Team.query.get(user.team_id).members if team else []
     return render_template('index.html', user=user, team_members=team_members,team_id=user.team_id, matching=user.matching,requested=user.requested)
 
-@app.route('/settings')
+@application.route('/settings')
 def settings():
     return render_template('settings.html')
 
 
-@app.route('/team_register', methods=['GET', 'POST'])
+@application.route('/team_register', methods=['GET', 'POST'])
 @login_required
 def team_register():
     if request.method == 'POST':
@@ -258,7 +260,7 @@ def team_register():
 
     return render_template('team_register.html')
 
-@app.route('/team_leave', methods=['POST'])
+@application.route('/team_leave', methods=['POST'])
 @login_required
 def team_leave():
     # 현재 사용자가 속한 팀을 가져옵니다.
@@ -275,7 +277,7 @@ def team_leave():
     flash("팀이 성공적으로 삭제되었습니다.")
     return redirect(url_for('index'))
 
-@app.route("/email", methods=['POST', 'GET'])
+@application.route("/email", methods=['POST', 'GET'])
 def email():
     if request.method == 'POST':
         receiver = request.form['email_receiver'] + '@khu.ac.kr'
@@ -307,7 +309,7 @@ def send_email(recipients, content):
         print(f"An error occurred: {e}")
         return str(e)
     
-@app.route("/email_verification", methods=['POST', 'GET'])
+@application.route("/email_verification", methods=['POST', 'GET'])
 def email_verificiation():
     entered_code = request.form['enter_code']
     stored_code = session.get('verification_code')
@@ -317,7 +319,7 @@ def email_verificiation():
     else:
         return render_template('email_verification.html', content="인증코드가 올바르지 않습니다")
 
-@app.route('/match_teams', methods=['POST'])
+@application.route('/match_teams', methods=['POST'])
 @login_required
 def match_teams():
     current_team_id = current_user.team_id
@@ -374,7 +376,7 @@ def match_teams():
         "current_team_result": matched_result_current_team
     })
 
-@app.route('/fetch_matching_status', methods=['GET'])
+@application.route('/fetch_matching_status', methods=['GET'])
 @login_required
 def fetch_matching_status():
     current_team_id = current_user.team_id
@@ -413,7 +415,7 @@ def fetch_matching_status():
     # Not requested
     return jsonify({"message": "매칭하기 버튼을 눌러 과팅을 시작해보세요", "requested": requested, "matching": matching})
 
-@app.route('/cancel_match', methods=['POST'])
+@application.route('/cancel_match', methods=['POST'])
 @login_required
 def cancel_match():
     current_team_id = current_user.team_id
@@ -434,21 +436,43 @@ def cancel_match():
 
     return jsonify({"message": "매칭 요청이 취소되었습니다."})
 
-@app.route('/chatroom')
+@application.route('/chatroom')
 def chatroom():
     return render_template('chatroom.html')
 
-@socketio.on('send_message')
-def handle_message(data):
-    print('Received message:', data)
-    socketio.emit('receive_message', data)
+# 채팅방과 메시지 매핑
+chat_rooms = defaultdict(lambda: defaultdict(list))
 
-@app.route('/get_user_info', methods=['GET'])
+@application.route('/send_message', methods=['POST'])
+def send_message():
+    data = request.json
+    room = data.get('room')
+    user = data.get('from')
+    message = data.get('message')
+
+    if room and user and message:
+        chat_rooms[room][user].append(message)  # 방과 사용자별로 메시지 추가
+        return jsonify({'status': 'Message received'})
+    return jsonify({'status': 'Invalid data'}), 400
+
+@application.route('/receive_message/<room>', methods=['GET'])
+def receive_message(room):
+    while True:
+        if chat_rooms[room]:  # 새로운 메시지가 있으면
+            messages = []
+            for user, msgs in chat_rooms[room].items():
+                if msgs:
+                    messages.extend({'user': user, 'message': msg} for msg in msgs)
+                    chat_rooms[room][user] = []  # 메시지를 반환한 후 삭제
+            return jsonify(messages)  # 모든 메시지를 반환
+
+        time.sleep(1)  # 메시지가 없으면 1초 대기 후 다시 체크
+
+@application.route('/get_user_info', methods=['GET'])
 @login_required
 def get_user_info():
     user = current_user
-    return jsonify({'username': user.username})
-
+    return jsonify({'user_id': user.userid, 'user_name': user.username, 'chat_room': f'room{user.team_id}'})
     
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    application.run()
